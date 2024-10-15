2d73b18ae287b9cace8360d33d765f44ebbcb1e3
augustsletto <augustsletto@gmail.com>
2024-10-15 17:02:52 +0200

    tkinter miles to km

--- venv/main.py
+++ venv/main.py
@@ -1,4 +1,53 @@
-import random
+from tkinter import *
+
+def miles_to_km():
+    miles = float(miles_input.get())
+    km = round(miles * 1.609)
+    kilometer_result_label.config(text=f"{km}")
 
 
-random.randint(1,2)
+
+
+
+
+
+window = Tk()
+window.title("UnitConverter")
+window.config(padx=20, pady=20)
+
+
+
+
+
+
+miles_input = Entry(width=7)
+miles_input.grid(column=1, row=0)
+
+miles_label = Label(text="Miles")
+miles_label.grid(column=2, row=0)
+
+is_equal_label = Label(text="is equal to")
+is_equal_label.grid(column=0, row=1)
+
+kilometer_result_label = Label(text="0")
+kilometer_result_label.grid(column=1, row=1)
+
+kilometer_label = Label(text="Km")
+kilometer_label.grid(column=2, row=1)
+
+
+calcualte_button = Button(text="Calculate", command=miles_to_km)
+calcualte_button.grid(column=1, row=2)
+
+
+
+
+
+
+
+
+
+
+
+
+window.mainloop()
